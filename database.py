cdf7aa64579c069f7d49ed0fc68e3f6be0b751e8
Andy Duarte <andyduarte@gmail.com>
2020-11-10 13:51:47 -0600

    start of the database

---
+++ database.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+con = sqlite3.connect('highscore.db')
+cur = con.cursor()
+try:
+    cur.execute("""CREATE TABLE HighScore(
+                Name TEXT,
+                Wave INTEGER,
+                Kills INTEGER
+                )""")
+except:
+    pass
+
+con.commit()
+def insert_score(score):
+    with con:
+        cur.execute('INSERT INTO HighScore VALUES (:Name,:Wave,:Kills)',{'Name': score.Name, 'Wave':score.Wave, 'Kills': score.Kills})
+con.commit()
+cur.execute('SELECT Name, Wave, Kills FROM HighScore')
+print(cur.fetchall())
+
+class score():
+    def __init__(self,Name, Wave, Kills):
+        self.Name = Name 
+        self.Wave = Wave 
+        self.Kills = Kills
+con.close()
