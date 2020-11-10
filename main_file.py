a39210185b5458987ac2a029e5b747598b008a08
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2020-11-10 11:03:49 -0600

    added gun variant functionality

--- main_file.py
+++ main_file.py
@@ -27,22 +27,37 @@ spawners = pygame.sprite.Group()
 all_sprites.add(spawner)
 spawners.add(spawner)
 
+store = Store((100, 100))
+all_sprites.add(store)
+
 running = True
 
 bullet_damage = 1
+is_auto = False
+is_shotgun = False
+gun_timer = 0
+timer_limit = 20
 
 money = 0
 
-while running:
 
-    # zombie.update(player, all_sprites)
+def use_shotgun():
+    for i in range(5):
+        new_bullet = Bullet((player.rect.left + 12 - i, player.rect.top + 12 - i), player.angle)
+        bullets.add(new_bullet)
+        all_sprites.add(new_bullet)
+while running:
+    pressed_keys = pygame.key.get_pressed()
     screen.fill((0, 25, 0))
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_g:
-                new_bullet = Bullet((player.rect.left + 12, player.rect.top + 12), player.angle)
-                bullets.add(new_bullet)
-                all_sprites.add(new_bullet)
+            if event.key == pygame.K_g and is_auto == False:
+                if is_shotgun == False:
+                    new_bullet = Bullet((player.rect.left + 12, player.rect.top + 12), player.angle)
+                    bullets.add(new_bullet)
+                    all_sprites.add(new_bullet)
+                else:
+                    use_shotgun()
             elif event.key == pygame.K_r:
                 player.angle -= 1
             elif event.key == pygame.K_t:
@@ -54,9 +69,33 @@ while running:
                 player.angle = 7
         if event.type == pygame.QUIT:
             running = False
+    if pressed_keys[K_g] and is_auto == True and is_shotgun == False:
+        gun_timer += 1
+        if gun_timer > timer_limit:
+            new_bullet = Bullet((player.rect.left + 12, player.rect.top + 12), player.angle)
+            bullets.add(new_bullet)
+            all_sprites.add(new_bullet)
+            timer = 0
+    elif pressed_keys[K_g] and is_auto == True and is_shotgun == True:
+        gun_timer += 1
+        if gun_timer > timer_limit:
+            use_shotgun()
+            gun_timer = 0
+    else:
+        gun_timer = 0
     
-    pressed_keys = pygame.key.get_pressed()
     player.update(pressed_keys)
+    if pygame.sprite.pygame.sprite.collide_rect(player, store):
+        redeemed = 0
+        redeemed = store.update(pressed_keys, money, is_auto, is_shotgun)
+        if redeemed == 1:
+            bullet_damage += 1
+        elif redeemed == 2:
+            is_auto = True
+        elif redeemed == 3:
+            is_shotgun = True
+
+
     for b in bullets:
         b.update()
     for enemy in enemies_group:
